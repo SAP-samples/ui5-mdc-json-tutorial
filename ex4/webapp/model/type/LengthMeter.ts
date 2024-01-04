3a468bfe96f78079cd3e0bdded29fd5a3277d6ce
Benedikt Schölch <benedikt.schoelch@sap.com>
2024-01-04 10:32:34 +0100

    ex4: added number type parameter definition to LengthMeter

--- ex4/webapp/model/type/LengthMeter.ts
+++ ex4/webapp/model/type/LengthMeter.ts
@@ -2,7 +2,7 @@ import Integer from "sap/ui/model/type/Integer"
 import NumberFormat from "sap/ui/core/format/NumberFormat"
 
 export default class LengthMeter extends Integer {
-    formatValue(iHeight) {
+    formatValue(iHeight: number) {
         const oUnitFormat = NumberFormat.getUnitInstance()
         return oUnitFormat.format(iHeight, "length-meter")
     }
