3d86f8c2a5e76c3673a0e4bb1cbca9e50dcccc9d
Benedikt Schölch <benedikt.schoelch@sap.com>
2024-12-12 19:55:36 +0100

    fix model name

--- ex7/webapp/view/MountainsTemplate.ts
+++ ex7/webapp/view/MountainsTemplate.ts
@@ -3,7 +3,6 @@ import "sap/ui/core/mvc/View";
 import View from "sap/ui/core/mvc/View";
 import XMLView from "sap/ui/core/mvc/XMLView";
 import JSONPropertyInfo from "../model/metadata/JSONPropertyInfo";
-import JSONModel from "sap/ui/model/json/JSONModel";
 
 /**
  * @namespace mdc.tutorial.view
@@ -15,9 +14,9 @@ export default class Mountains extends View {
             preprocessors: {
                 xml: {
                     models: {
-                        pim: new JSONModel({
-                                PropertyInfo: JSONPropertyInfo
-                        })
+                        pim: {
+                            PropertyInfo: JSONPropertyInfo
+                        }
                     }
                 }
             },
